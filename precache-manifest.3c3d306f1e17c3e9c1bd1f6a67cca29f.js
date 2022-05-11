509d6daa3b12a28cd226ad84c26dee5fa572ddb9
Menderes <mndrs.kc@gmail.com>
2022-05-11 18:59:43 -0400

    Updates

---
+++ precache-manifest.3c3d306f1e17c3e9c1bd1f6a67cca29f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1337dcec9a8cf40b9d428437c8b6f3b0",
+    "url": "/MenderesKoc/react-portfolio.git/index.html"
+  },
+  {
+    "revision": "b1bde97554f9e95a94fe",
+    "url": "/MenderesKoc/react-portfolio.git/static/css/main.a9d28235.chunk.css"
+  },
+  {
+    "revision": "15792bb0d97d4e8f1be4",
+    "url": "/MenderesKoc/react-portfolio.git/static/js/2.ac3d2b48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MenderesKoc/react-portfolio.git/static/js/2.ac3d2b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bde97554f9e95a94fe",
+    "url": "/MenderesKoc/react-portfolio.git/static/js/main.ff254934.chunk.js"
+  },
+  {
+    "revision": "fc97f578d1f383a21779",
+    "url": "/MenderesKoc/react-portfolio.git/static/js/runtime-main.85164544.js"
+  },
+  {
+    "revision": "0410da14518ecc8c9e271cfaa041fcc7",
+    "url": "/MenderesKoc/react-portfolio.git/static/media/MenderesKocResume.0410da14.docx"
+  }
+]);
