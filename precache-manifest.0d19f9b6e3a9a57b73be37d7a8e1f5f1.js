8d0065648a83662f0d6691232dca6c5948f1372f
Menderes <mndrs.kc@gmail.com>
2022-05-12 02:57:48 -0400

    Updates

---
+++ precache-manifest.0d19f9b6e3a9a57b73be37d7a8e1f5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e707b14568e4049116491d9ca1399f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "85a89275d6b4fa6f50ab",
+    "url": "/react-portfolio/static/css/main.a9d28235.chunk.css"
+  },
+  {
+    "revision": "b9daa8a38f6761dad3a7",
+    "url": "/react-portfolio/static/js/2.1daed910.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.1daed910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a89275d6b4fa6f50ab",
+    "url": "/react-portfolio/static/js/main.ae4bc4d6.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "0410da14518ecc8c9e271cfaa041fcc7",
+    "url": "/react-portfolio/static/media/MenderesKocResume.0410da14.docx"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MenderesKoc/react-portfolio/precache-manifest.dfe2ba0818c84f5948446b148664aa24.js"
+  "/react-portfolio/precache-manifest.0d19f9b6e3a9a57b73be37d7a8e1f5f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MenderesKoc/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
