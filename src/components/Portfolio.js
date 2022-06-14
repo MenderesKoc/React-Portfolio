fe3721508083e65f57867a82c7d401a98ad76fc8
Menderes Koc <mndrs.kc@gmail.com>
2022-06-14 14:42:25 -0400

    created portfolio secction

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,49 @@
 import React from "react";
 
-function ProjectCards(props) {
+function Portfolio() {
+  const projects = [
+    {
+      project_number: "project4",
+      title: "TASKMASTER PRO",
+      githubUrl: "https://github.com/mendereskoc/taskmaster-pro",
+      live_url: "https://mendereskoc.github.io/taskmaster-pro/",
+      description: "Edit, organize, and manage tasks",
+    },
+  ];
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <p className="project-title" id="card-title">
-          {props.name}
-        </p>
-        menderess
-        <p>
-          <a href={props.github}>
-            <img
-              src="https://img.icons8.com/color/48/000000/github--v1.png"
-              alt="Repository"
-              id="project-icon"
-            />
-          </a>
-          <a href={props.deploy}>
-            <img
-              src="https://img.icons8.com/color/48/000000/monitor.png"
-              alt="Deployed Site"
-              id="project-icon"
-            />
-          </a>
-        </p>
-        <p id="topics">({props.topics})</p>
-      </div>
-    </div>
+    <article class="projects" id="projects">
+      <section id="projects-tag">
+        <h1 class="project-title">PROJECTS</h1>
+      </section>
+
+      <section class="projects-content">
+        {projects.map((data) => (
+          <button class="btn" id="hoverbig" value="PLACEHOLDER">
+            <section class={data.project_number} id="allcards">
+              <p class="cards-title">{data.title}</p>
+              <div class="button-holder">
+                <img
+                  id="hoverbig"
+                  onClick={() => window.open(data.githubUrl, "_blank")}
+                  class="github3"
+                  alt="linkedin link"
+                />
+                <button
+                  class="liveurl"
+                  id="hoverbig"
+                  onClick={() => window.open(data.live_url, "_blank")}
+                  value="PLACEHOLDER"
+                >
+                  LIVE URL
+                </button>
+              </div>
+              <p class="cards">{data.description}</p>
+            </section>
+          </button>
+        ))}
+      </section>
+    </article>
   );
 }
 
-export default ProjectCards;
+export default Portfolio;
